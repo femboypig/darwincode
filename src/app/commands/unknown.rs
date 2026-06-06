88ec341ed1ff1c18037ced04ea568fb917b9922a
femboypig <harimassaa@gmail.com>
2026-06-07 00:31:19 +0400

    Add unit test for unknown command run function

--- src/app/commands/unknown.rs
+++ src/app/commands/unknown.rs
@@ -7,3 +7,16 @@ pub fn run(app: &mut App, command: String) {
     )));
     app.status = "Unknown command".to_owned();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::StoredConfig;
+
+    #[test]
+    fn test_unknown_run() {
+        let mut app = App::new(Some(StoredConfig::default()));
+        run(&mut app, "/invalid".to_owned());
+        assert_eq!(app.status, "Unknown command");
+    }
+}
