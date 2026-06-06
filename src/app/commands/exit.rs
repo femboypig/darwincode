9543f37a20e3f44acff0f2c78701724741efe4fc
femboypig <harimassaa@gmail.com>
2026-06-07 00:30:45 +0400

    Add unit test for exit command run function

--- src/app/commands/exit.rs
+++ src/app/commands/exit.rs
@@ -3,3 +3,17 @@ use crate::app::core::App;
 pub fn run(app: &mut App) {
     app.should_quit = true;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::StoredConfig;
+
+    #[test]
+    fn test_exit_run() {
+        let mut app = App::new(Some(StoredConfig::default()));
+        app.should_quit = false;
+        run(&mut app);
+        assert!(app.should_quit);
+    }
+}
